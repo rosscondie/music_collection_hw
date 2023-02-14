ccf55588912c2feaec195c9c404de97591d8a57d
Ross Condie <rosscondie@gmail.com>
2023-02-14 20:13:03 +0100

    added CRUD functions to album_repository

--- console.py
+++ console.py
@@ -2,9 +2,17 @@ import pdb
 from models.album import Album
 from models.artist import Artist
 import repositories.album_repository as album_repository
-import repositories.artist_repository as artist_repository
+# import repositories.artist_repository as artist_repository
 
 album_repository.delete_all()
-artist_repository.delete_all()
+# artist_repository.delete_all()
+
+# album_1 = Album("Californication", "Rock")
+# album_repository.save(album_1)
+
+# result = album_repository.select_all()
+
+# for album in result:
+#     print(album.__dict__)
 
 pdb.set_trace()
