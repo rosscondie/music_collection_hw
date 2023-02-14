cf6678eab027e7ac085fe3de4533897e26dde665
Ross Condie <rosscondie@gmail.com>
2023-02-14 21:49:35 +0100

    final commit

--- console.py
+++ console.py
@@ -5,13 +5,15 @@ import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
 album_repository.delete_all()
-# artist_repository.delete_all()
+artist_repository.delete_all()
 
-album_1 = Album("Californication", "Rock")
+artist_1 = Artist("Elvis", "Presley")
+artist_repository.save(artist_1)
+
+
+album_1 = Album("Jailhouse Rock", "Rock")
 album_repository.save(album_1)
 
-artist_1 = Artist("Anthony", "Kiedes")
-artist_repository.save(artist_1)
 
 result = album_repository.select_all()
 
